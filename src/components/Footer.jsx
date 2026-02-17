da008f479e9588c6c257f43cef6e9b7c635822a7
Julio Cesar <jcesar206@hotmail.com>
2026-02-17 11:33:37 -0600

    Footer.jsx: Se actualiza diseño y logica del componente

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,21 +12,21 @@ const Footer = () => {
     	<div className="max-w-6xl mx-auto px-6 py-6 flex flex-col items-center gap-6">
 					{/*Social icons*/}
 					<div className="flex gap-6">
-				<a href="https://jcesar206.github.io/myPersonalBlog/" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-300 hover:text-black transition" title="My Personal Blog">
+				<a href="https://jcesar206.github.io/myPersonalBlog/" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-300 hover:text-black dark:hover:text-gray-600 transition" title="My Personal Blog">
 					<FaHome size={20}/>
 				</a>
 				<a href="https://github.com/JCesar206" target="_blank" rel="noreferrer"
-					className="text-gray-600 dark:text-gray-300 hover:text-black transition" title="Github">
+					className="text-gray-600 dark:text-gray-300 hover:text-black dark:hover:text-gray-600 transition" title="Github">
           <FaGithub size={20}/>
         </a>
 				<a href="https://www.linkedin.com/in/jcesar206" target="_blank" rel="noreferrer"
-					className="text-gray-600 dark:text-gray-300 hover:text-black transition" title="LinkedIn">
+					className="text-gray-600 dark:text-gray-300 hover:text-black dark:hover:text-gray-600 transition" title="LinkedIn">
           <FaLinkedin size={20}/>
         </a>
         <a href="mailto:jcesar206@hotmail.com" className="text-gray-600 dark:text-gray-300 
-				hover:text-black transition" title="Hotmail"><FaEnvelope size={20}/></a>
+				hover:text-black dark:hover:text-gray-600 transition" title="Hotmail"><FaEnvelope size={20}/></a>
 				<a href="mailto:jcesary06@gmail.com" className="text-gray-600 dark:text-gray-300 
-				hover:text-black transition" title="Gmail"><SiGmail size={20}/></a>
+				hover:text-black dark:hover:text-gray-600 transition" title="Gmail"><SiGmail size={20}/></a>
 			</div>
 			{/*Copyright*/}
 			<p className="text-sm text-gray-500 dark:text-gray-400 text-center font-bold">&copy; {new Date().getFullYear()} {t.copyright} | Juls | {t("copy")}</p>
